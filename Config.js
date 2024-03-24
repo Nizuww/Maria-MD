f749377048dd72bfe964426acee3beaf6444730d
Nizuww <157577486+Nizuww@users.noreply.github.com>
2024-03-24 06:17:24 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['94753723108']
+global.ownername = "💙𓆩‎𝗗𝗔𝗫𝗧𝗢𝗡 𝗜𝗗"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
@@ -11,9 +11,9 @@ global.location = "Indian"
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '💙𓆩‎𝗗𝗔𝗫𝗧𝗢𝗫 𝗜𝗗𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = '𝗗𝗔𝗫𝗧𝗢𝗡 Bot'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
@@ -26,13 +26,13 @@ global.prefa = ['!']
 
 //false=disable and true=enable
 global.welcome = false //auto welcome
-global.autoRecording = false //auto recording
+global.autoRecording = true //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti212 = true //auto block +212
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 
 
